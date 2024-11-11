70e8903d38554fc4cb56537222c14f7cb4e9ef3e
ParkSGSG <psg4160@gmail.com>
2024-11-12 08:28:53 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 2.18 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -1,4 +1,5 @@
-def solution(food): # 0을 기준으로 앞 숫자 작성 후 0과 역순을 더하기
+"""# 1. 0을 기준으로 앞 숫자 작성 후 0과 역순을 더하기
+def solution(food):  
     answer = ''
     for i in range(1,len(food)):
         k = int(food[i]) // 2
@@ -8,4 +9,15 @@ def solution(food): # 0을 기준으로 앞 숫자 작성 후 0과 역순을 더
     re = answer[::-1]
     answer += '0'
     answer += re
+    return answer
+"""
+# 2 역순으로 두개씩 중앙(0)에서부터 배치하기
+
+def solution(food):
+    answer ="0"
+    for i in range(len(food)-1, 0,-1):
+        c = int(food[i]/2)
+        while c>0:
+            answer = str(i) + answer + str(i)
+            c -= 1
     return answer
